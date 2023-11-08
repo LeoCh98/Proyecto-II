9ccf3b80242b9e6ef07df9ab6d55f7183fceb952
LeoCh98 <68918387+LeoCh98@users.noreply.github.com>
2023-11-07 22:10:59 -0600

    Initial

---
+++ ProyectoII-backend/src/main/java/com/progra/proyectoii/backend/ProyectoIIBackend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.progra.proyectoii.backend;
+
+/**
+ *
+ * @author leoch
+ */
+public class ProyectoIIBackend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
