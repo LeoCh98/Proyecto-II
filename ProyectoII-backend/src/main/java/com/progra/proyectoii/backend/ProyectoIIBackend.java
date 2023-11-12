715e1f59db7eadf14838327d872d5d4d01a25c5e
LeoCh98 <68918387+LeoCh98@users.noreply.github.com>
2023-11-11 23:15:28 -0600

    Successful connection to db...

--- ProyectoII-backend/src/main/java/com/progra/proyectoii/backend/ProyectoIIBackend.java
+++ ProyectoII-backend/src/main/java/com/progra/proyectoii/backend/ProyectoIIBackend.java
@@ -1,16 +1,35 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.progra.proyectoii.backend;
+
+import cr.ac.una.db.Database;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  *
  * @author leoch
  */
+
+// Just for ensure access to the db... Delete later.
+
 public class ProyectoIIBackend {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Database db = Database.createMySQLDatabase("db.properties");
+            System.out.println(db);
+
+            System.out.println("Connecting..");
+            try (Connection cnx = db.getConnection()) {
+                System.out.println(cnx);
+                System.out.println("Successful connection..");
+            }
+            System.out.println("Connection closed.");
+
+        } catch (SQLException ex) {
+            System.err.printf("Exception: '%s'%n", ex.getMessage());
+        }
     }
 }
