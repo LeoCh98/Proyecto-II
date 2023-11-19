a7d9aafef053078cf02f4e74f02160d2b491a52a
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2023-11-18 18:10:19 -0600

    Configuring REST

--- ProyectoII-backend/src/main/java/com/mycompany/proyectoii/backend/JakartaRestConfiguration.java
+++ ProyectoII-backend/src/main/java/com/mycompany/proyectoii/backend/JakartaRestConfiguration.java
@@ -1,13 +1,30 @@
 package com.mycompany.proyectoii.backend;
 
+import com.mycompany.proyectoii.backend.resources.*;
+import jakarta.annotation.security.DeclareRoles;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
  * @author Juneau
  */
-@ApplicationPath("resources")
+@ApplicationPath("api")
+//@DeclareRoles({"true", "false"})
 public class JakartaRestConfiguration extends Application {
-    
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> classes = new HashSet<>();
+        /*
+        classes.add(Policies.class);
+        classes.add(Categories.class);
+        classes.add(Coverages.class);
+        classes.add(Login.class);
+        classes.add(Vehicles.class);
+        classes.add(Clients.class);
+        */
+        return classes;
+    } 
 }
